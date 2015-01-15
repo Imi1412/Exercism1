e0848ffb7884876145e8a26dad6c3a60aed2b4f5
Imi1412 <numberunobigsister@gmail.com>
2015-01-15 12:30:21 -0800

    Exercism1

---
+++ First Program/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a Year: ");
+            int year = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Year.IsLeap(year));
+            Console.Read();
+        }
+    }
+}
---
+++ First Program/Year.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    public class Year
+    {
+        public static bool IsLeap(int year)
+        {
+            bool isLeap = false;
+
+            if (year % 4 == 0 && year % 100 != 0)
+            {
+                isLeap = true;
+            }
+
+            if (year % 100 == 0 && year % 400 == 0)
+            {
+                isLeap = true;
+            }
+            
+            return isLeap;
+        }
+
+    }
+
